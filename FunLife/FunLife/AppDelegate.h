85f32e6992a1b9e267025dc0fbfc8020f24ec71f
huangjian <huangjian@182.92.149.44>
2016-03-15 16:37:18 +0800

    提交工程

---
+++ FunLife/FunLife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunLife
+//
+//  Created by huangjian on 16/3/15.
+//  Copyright © 2016年 huangjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
