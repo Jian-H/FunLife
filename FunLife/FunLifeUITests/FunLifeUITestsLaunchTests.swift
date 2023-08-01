8bf3b6aa5dd35507b05fedc6083d7810b913c3d1
Jian-H <1026643065@qq.com>
2023-08-01 21:52:25 +0800

    新工程

---
+++ FunLife/FunLifeUITests/FunLifeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FunLifeUITestsLaunchTests.swift
+//  FunLifeUITests
+//
+//  Created by fangtianios on 2023/8/1.
+//
+
+import XCTest
+
+final class FunLifeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
